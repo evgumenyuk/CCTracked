12ee090ce126eb2a1aa10565a603f3786982445e
ev-gumenyuk <ev.gumenyuk@gmail.com>
2019-07-04 00:12:45 +0300

    asasdas

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class CCTracker extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.red
+        primarySwatch: Colors.blue
       ),
       home: CCList(),
     );
